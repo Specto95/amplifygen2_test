cb8517cdc1a8b39a0bc83bc76c3d89d9230e5a74
EnriqueOrtiz95 <enortizlo@ittepic.edu.mx>
2024-09-30 14:36:05 -0700

    Changing auth config to Sign-In Google --> WIP

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -8,7 +8,6 @@ export const auth = defineAuth({
   loginWith: {
     email: true,
     externalProviders: {
-      scopes: ["EMAIL"],
       google: {
         clientId: secret("GOOGLE_CLIENT_ID"),
         clientSecret: secret("GOOGLE_CLIENT_SECRET"),
@@ -21,6 +20,7 @@ export const auth = defineAuth({
         "http://localhost:5173/",
         "https://main.d1tqji0orytvis.amplifyapp.com/",
       ],
+      scopes: ["EMAIL"],
     },
   },
 });
